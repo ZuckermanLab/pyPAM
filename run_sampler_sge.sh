5639ac9d2d7441db179fedeb76fc2a600616f503
August <30163079+augeorge@users.noreply.github.com>
2022-06-30 08:44:30 -0700

    Create run_sampler_sge.sh

---
+++ run_sampler_sge.sh
@@ -0,0 +1,17 @@
+NEED TO CONVERT FROM SLURM to SGE: https://srcc.stanford.edu/sge-slurm-conversion 
+
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=20 
+#SBATCH --mem-per-cpu=10GB 
+#SBATCH --job-name=parallel_affine
+#SBATCH --time=35:00:00 
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=<email address goes here>
+#SBATCH --output=./out/%j.out
+#SBATCH --error=./err/%j.err
+
+source /home/path/to/python/environment/bin/activate
+
+srun python /home/path/to/parallel_affine/parallel_affine_example.py 
