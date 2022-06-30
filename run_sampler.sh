33e477633cd876065c5e9c260ac10730ddb2ae02
August <30163079+augeorge@users.noreply.github.com>
2022-06-30 08:41:19 -0700

    Update run_sampler.sh

--- run_sampler.sh
+++ run_sampler.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=1
-#SBATCH --cpus-per-task=20 ## 10
-#SBATCH --mem-per-cpu=10GB ## 10 GB 
-#SBATCH --job-name=affine
-#SBATCH --time=35:00:00 ## 9-23:00:00 ask for 9 days + 23 hours hr on the node
-##SBATCH --qos=long_jobs
-#SBATCH --partition=exacloud
+#SBATCH --cpus-per-task=20 
+#SBATCH --mem-per-cpu=10GB 
+#SBATCH --job-name=parallel_affine
+#SBATCH --time=35:00:00 
 #SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=georgeau@ohsu.edu
+#SBATCH --mail-user=<email address goes here>
 #SBATCH --output=./out/%j.out
 #SBATCH --error=./err/%j.err
 
-source /home/groups/ZuckermanLab/georgeau/sampling_tests/py36_emcee/bin/activate
+source /home/path/to/python/environment/bin/activate
 
-srun python /home/groups/ZuckermanLab/georgeau/sampling_tests/parallel_affine/parallel_affine_example.py 
+srun python /home/path/to/parallel_affine/parallel_affine_example.py 
