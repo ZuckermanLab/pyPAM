d819a997782f1deaf0eeb4d7472ea13f01fb17de
August <30163079+augeorge@users.noreply.github.com>
2023-04-07 03:14:34 -0700

    updates

--- pyPAM/parallel_affine.py
+++ pyPAM/parallel_affine.py
@@ -9,6 +9,37 @@ import pyPAM.parallel_affine_utility as pau
 
 
 class ParallelEnsembleSampler:
+    """Parallel affine invariant ensemble sampler for MCMC.
+
+    This class implements the parallel affine invariant ensemble sampler algorithm for Markov Chain Monte Carlo (MCMC) simulations. It is designed to be run on multiple processors or cores, and is intended for use with high-dimensional problems that are difficult to sample from using traditional MCMC methods.
+
+    Args:
+        n_ensembles (int): The number of independent ensembles to run.
+        n_walkers (int): The total number of walkers across all ensembles. Should be an even multiple of n_dim.
+        n_dim (int): The number of dimensions in the problem space.
+        log_prob (function): A function that computes the log probability of a given point in the problem space.
+        log_prob_args (list): A list of additional arguments to pass to log_prob.
+        thin (int): The thinning factor to use for saving samples. Only every `thin`th sample will be saved.
+        backend_fnames (list): A list of file names for the backends to use. There should be one filename for each ensemble.
+        moves (list): A list of moves to use for each ensemble.
+
+    Attributes:
+        n_ensembles (int): The number of independent ensembles to run.
+        n_walkers (int): The total number of walkers across all ensembles.
+        n_dim (int): The number of dimensions in the problem space.
+        log_prob (function): A function that computes the log probability of a given point in the problem space.
+        log_prob_args (list): A list of additional arguments to pass to log_prob.
+        thin (int): The thinning factor to use for saving samples.
+        backend_fnames (list): A list of file names for the backends to use.
+        moves_list (list): A list of moves to use for each ensemble.
+        backend_list (list): A list of backends for each ensemble.
+        sampler_list (list): A list of ensemble samplers for each ensemble.
+
+    Raises:
+        AssertionError: If any of the input arguments are invalid.
+
+    """
+
     def __init__(self, n_ensembles, n_walkers, n_dim, log_prob, log_prob_args, thin, backend_fnames, moves):
         """initializes sampler class"""
         self.n_ensembles = n_ensembles
@@ -20,7 +51,6 @@ class ParallelEnsembleSampler:
         self.backend_fnames = backend_fnames
         self.moves_list = moves
       
-
         # check that inputs are valid
         assert(type(self.n_ensembles)==int and self.n_ensembles>=1)
         assert(type(self.n_dim)==int and self.n_dim>=1)
@@ -30,7 +60,6 @@ class ParallelEnsembleSampler:
         assert(type(self.thin)==int and self.thin>=1)
         assert(type(self.backend_fnames)==list and len(self.backend_fnames)==self.n_ensembles)
         assert(type(self.moves_list)==list and len(self.moves_list)==self.n_ensembles)
-
 
         # Initialize the sampler
         # create a list ('ensemble') of backends and affine invariant ensemble samplers
@@ -45,15 +74,40 @@ class ParallelEnsembleSampler:
 
     
     def update_backends(self, id):
-        """update the backend to include an additional section"""
+        """Update the backends to include a new section.
+
+        This method updates the HDF5 backend for each ensemble by adding a new section to the file. The new section is identified by an ID string, which is appended to the name of the section.
+
+        Args:
+            id (str): The ID string to use for the new section.
+
+        Raises:
+            AssertionError: If the input ID is not a string, or is an empty string.
+        """
         self.id = id
         assert(type(self.id)==str and len(self.id)>=1)
         self.backend_list = [emcee.backends.HDFBackend(self.backend_fnames[i], name=f"{self.id}_ensemble_{i}") for i in range(self.n_ensembles)] 
         assert(all(type(item) is emcee.backends.HDFBackend for item in self.backend_list)==True)  # check that list was created correctly
 
    
-    def run_sampler(self,p_0,n_steps,n_cores,thin,id):
-        """runs parallel ensemble samplers - without mixing"""
+    def run_sampler(self, p_0, n_steps, n_cores, thin, id):
+        """Run the parallel ensemble samplers.
+
+        This method runs the parallel affine invariant ensemble samplers for a specified number of steps, with a given initial state and number of cores. It updates the backends for each ensemble to include a new section identified by an ID string, and returns the final state of the samplers.
+
+        Args:
+            p_0 (ndarray): The initial state for each walker in each ensemble, as a 3D array with shape (n_ensembles, n_walkers, n_dim).
+            n_steps (int): The number of steps to run each sampler for.
+            n_cores (int): The number of CPU cores to use for parallelization.
+            thin (int): The thinning factor to use for saving samples.
+            id (str): The ID string to use for the new backend section.
+
+        Returns:
+            state_list (list): A list of sampler states, one for each ensemble.
+
+        Raises:
+            AssertionError: If any of the input arguments are invalid.
+        """
         # note: self.n_steps gets updated during self.run_sampler() call
         self.p_0 = p_0
         self.n_steps = n_steps
@@ -83,28 +137,53 @@ class ParallelEnsembleSampler:
         self.sampler_list = new_sampler_list  # update self w/ latest sampling results
         self.backend_list = new_backend_list # update self w/ latest backend results
         return state_list  
-
+    
 
     def get_chains(self):
-        """returns the MCMC chains (samples) w/ shape = (n_ensembles, n_steps, n_walkers, n_dim)"""
+        """Return the MCMC chains (samples) for each ensemble.
+
+        This method returns the MCMC chains (samples) for each ensemble as a 4D numpy array with shape (n_ensembles, n_steps, n_walkers, n_dim).
+
+        Returns:
+            samples (ndarray): The MCMC chains for each ensemble.
+
+        Raises:
+            AssertionError: If the shape of the returned samples is not as expected.
+        """
         samples = [i.get_chain() for i in self.sampler_list]
         assert(np.shape(samples)==(self.n_ensembles,int(self.n_steps/self.thin),self.n_walkers,self.n_dim))
         return np.array(samples)
 
 
     def get_last_samples(self):
-        """returns the last walker positions (last sample points) for each ensemble w/ shape = (n_ensembles, n_walkers, n_dim) """
+        """Return the last walker positions for each ensemble.
+
+        This method returns the last walker positions (last sample points) for each ensemble as a 3D numpy array with shape (n_ensembles, n_walkers, n_dim).
+
+        Returns:
+            last_samples (ndarray): The last walker positions for each ensemble.
+
+        Raises:
+            AssertionError: If the last samples returned by the method are not correct.
+        """
         samples = self.get_chains()  # shape = (n ensembles, n steps/walker, n walkers, n dim)
         last_samples = samples[:,-1]
         # check that last samples are correct. - use alternative way to get last samples using emcee sampler 
         last_samples_emcee = np.array([i.get_last_sample()[0] for i in self.sampler_list])
         assert(np.shape(last_samples)==np.shape(last_samples_emcee))
-        assert(np.array_equal(last_samples,last_samples_emcee))  # might be slow for large arrays
+        assert(np.all_close(last_samples,last_samples_emcee))  # can use array_equal for slower but more accurate method
         return last_samples
 
 
     def mix_ensembles(self):
-        """shuffle last examples to use as initial walker positions for next sampling run w/ shape = (n_ensembles, n_walkers, n_dim)"""
+        """Shuffle the last samples to use as initial walker positions for the next sampling run.
+
+        This method shuffles the last samples from each ensemble sampler and combines them into a new set of initial walker positions for the next sampling run.
+
+        Returns:
+            p0_new (ndarray): The new set of initial walker positions, as a 3D numpy array with shape (n_ensembles, n_walkers, n_dim).
+        """
+
         last_samples = self.get_last_samples()
         # combine last samples from the ensemple sampler, so each row is a walker coordinate 
         # old shape (n_ensembles, n_walkers, n_dim) --> new shape (n_ensembles*n_walkers, n_dim)
@@ -114,16 +193,31 @@ class ParallelEnsembleSampler:
         return p0_new  
 
 
-    def run_mixing_sampler(self,p_0,n_steps_list,n_cores,n_mixing_stages, thin, id_list):
-        """runs parallel ensemble samplers - with N mixing steps plus a final sampling run"""
+    def run_mixing_sampler(self, p_0, n_steps_list, n_cores, n_mixing_stages, thin, id_list):
+        """Run the parallel ensemble samplers with N mixing steps plus a final sampling run.
+
+        This method runs the parallel affine invariant ensemble samplers with a specified number of mixing steps, followed by a final sampling run. It takes an initial state, a list of numbers of steps to run at each mixing stage, the number of CPU cores to use for parallelization, the number of mixing stages to run, a thinning factor, and a list of ID strings for the backend sections for each mixing stage.
+
+        Args:
+            p_0 (ndarray): The initial state for each walker in each ensemble, as a 3D array with shape (n_ensembles, n_walkers, n_dim).
+            n_steps_list (list): A list of integers specifying the number of steps to run each sampler for at each mixing stage.
+            n_cores (int): The number of CPU cores to use for parallelization.
+            n_mixing_stages (int): The number of mixing stages to run.
+            thin (int): The thinning factor to use for saving samples.
+            id_list (list): A list of ID strings for the backend sections for each mixing stage.
+
+        Returns:
+            state_list (list): The final state of the samplers, as a list of sampler states.
+
+        Raises:
+            AssertionError: If any of the input arguments are invalid.
+        """
         # check that inputs are valid        
         assert(isinstance(p_0,(list,np.ndarray)) and np.shape(p_0) == (self.n_ensembles, self.n_walkers, self.n_dim))
         assert(type(n_steps_list)==list and all((type(item) is int) and (item > 0) for item in n_steps_list)==True)
         assert(type(n_cores)==int and n_cores>=1)
-        assert(type(n_mixing_stages)==int and n_mixing_stages>=0)
-        
+        assert(type(n_mixing_stages)==int and n_mixing_stages>=0)   
         assert(type(thin)==int and thin>=1)
- 
         assert(type(id_list)==list and len(id_list)==n_mixing_stages and (all(type(item) is str for item in id_list)==True))
         self.id_list = id_list
 
@@ -139,16 +233,27 @@ class ParallelEnsembleSampler:
 
 
     def get_flat_samples(self):
-        """return samples in a flat shape - (n_ensembles*n_steps*n_walkers, n_dim)"""
+        """Return the MCMC samples in a flat shape.
+
+        This method returns the MCMC samples in a flat shape, with dimensions (n_ensembles*n_steps*n_walkers, n_dim).
+
+        Returns:
+            flat_samples (ndarray): The flattened MCMC samples.
+
+        """
         chains = self.get_chains()
         flat_samples = np.reshape(chains, (self.n_ensembles*int(self.n_steps/self.thin)*self.n_walkers, self.n_dim))
         return flat_samples
 
 
-    
-
     def reset_backends(self):
-        """resets backend - removing previous samples"""
+        """Reset the backends, removing all previous samples.
+
+        This method resets the backends by removing all previous samples from the MCMC chains. This can be useful when starting a new sampling run from scratch.
+
+        Raises:
+            Warning: A warning message is printed to the console indicating that the backend has been reset.
+        """
         for b in self.backend_list:
             b.reset(self.n_walkers, self.n_dim)
             print('warning: resetting backend (removing old samples from chain)')
--- pyPAM/parallel_affine_utility.py
+++ pyPAM/parallel_affine_utility.py
@@ -7,7 +7,17 @@ import matplotlib.pyplot as plt
 
 
 def plot_single_ensemble_mixing_distributions(paie_sampler, fname='parallel_affine_example_dist', n_bins=100, xlim=[[-1,1]]):
-    """creates seperate distribution figures for each ensemble - using data from each parameter and mixing stage """
+    """Plots 1D marginal posterior distributions for each parameter and mixing stage in a separate figure for each ensemble.
+
+    Args:
+        paie_sampler (ParallelEnsembleSampler): A parallel ensemble sampler object.
+        fname (str): The filename to save the plot.
+        n_bins (int): The number of bins for the histogram.
+        xlim (list of list): The limits for the x-axis for each parameter.
+
+    Returns:
+        tuple: The figure and axes objects.
+    """
     assert(type(fname)==str and len(fname)>=1)
     assert(type(n_bins)==int and n_bins>1)
     assert(type(xlim)==list and xlim[0][1]>xlim[0][0])
@@ -26,7 +36,6 @@ def plot_single_ensemble_mixing_distributions(paie_sampler, fname='parallel_affi
                     ax.legend()
                 ax.set_title(f'p_{i} distribution')
                 ax.set_xlim(xlim[0][0], xlim[0][1])
-
         else:
             ax = axs
             i=0
@@ -36,16 +45,22 @@ def plot_single_ensemble_mixing_distributions(paie_sampler, fname='parallel_affi
                 ax.legend()
                 ax.set_title(f'p_{j} distribution')
             ax.set_xlim(xlim[i][0], xlim[i][1])
-
         plt.tight_layout()
         plt.savefig(f'{fname}_{k}.png')
         plt.close()
     return fig, axs
 
 
-
 def get_data_all_runs(paie_sampler, flat=False):
-    """gets data from multiple runs and multiple ensembles. Outputs a list of datasets. D[0] = data from all runs from ensemble 0. D[0][0] = data from run 0 of ensemble 0"""
+    """gets data from multiple runs and multiple ensembles. Outputs a list of datasets. D[0] = data from all runs from ensemble 0. D[0][0] = data from run 0 of ensemble 0
+    
+    Args:
+        paie_sampler: a ParallelEnsembleSampler object
+        flat: a boolean flag indicating whether the chains should be flattened
+        
+    Returns:
+        data_list: a list of numpy arrays, with each array containing the MCMC chain samples for an ensemble. The arrays are nested within a list, with the outer list containing arrays for each ensemble.
+    """
     s = paie_sampler
     backend_fnames = s.backend_fnames
     data_list = []
@@ -68,13 +83,20 @@ def get_data_all_runs(paie_sampler, flat=False):
 
 
 def get_data_dict_from_backend(fname, flat=False):
+    """Extracts MCMC chain data from an HDF5 backend file and returns it as a dictionary.
+    Args:
+        fname (str): The name of the HDF5 file to read.
+        flat (bool, optional): Whether to flatten the MCMC chains into a 2D array. Defaults to False.
+    
+    Returns:
+        dict: A dictionary where each key corresponds to a chain in the backend file, and the value is the chain data.
+    """
     assert(type(fname)==str and len(fname)>=1)
     f = h5py.File(fname,'r')
     ensemble_data_dict = {}
     for key in f.keys():
         if key != 'init_empty':
             c_tmp = f[key]['chain']
-            print(np.shape(c_tmp))
             if flat ==True:
                 c = np.reshape(c_tmp, (c_tmp.shape[0]*c_tmp.shape[1],c_tmp.shape[2])) 
             else:
@@ -86,7 +108,10 @@ def get_data_dict_from_backend(fname, flat=False):
 
 
 def print_backend_info(fname):
-    """prints info on the hdf5 file contents"""
+    """Prints information about the contents of an HDF5 backend file.
+    Args:
+        fname (str): The name of the HDF5 file to read.
+    """
     with h5py.File(fname, "r") as f:
         for key in list(f.keys()):
             print(f'dataset: {key}')
@@ -96,7 +121,22 @@ def print_backend_info(fname):
 
 
 def wrapper(arg_list):
-    """wrapper function to make and run ensemble sampler object in parallel"""
+    """Wrapper function to create and run an EnsembleSampler object in parallel.
+        Args:
+            arg_list (list): A list containing the arguments to initialize the EnsembleSampler object and run the MCMC sampling.
+                arg_list[0] (int): The number of walkers in the sampler.
+                arg_list[1] (int): The number of dimensions of the parameter space.
+                arg_list[2] (function): The log probability function to sample from.
+                arg_list[3] (tuple): The arguments to pass to the log probability function.
+                arg_list[4] (ndarray): The initial positions of the walkers in the parameter space.
+                arg_list[5] (int): The number of MCMC steps to take.
+                arg_list[6] (int): The thinning factor for the MCMC chain.
+                arg_list[7] (Backend): The backend object to store the MCMC chain data.
+                arg_list[8] (list): The list of move objects to use for the sampler.
+                
+        Returns:
+            tuple: A tuple containing the EnsembleSampler object, the final state of the sampler, and the backend object.
+        """
     n_walkers = arg_list[0]
     n_dim = arg_list[1]
     log_prob = arg_list[2]
