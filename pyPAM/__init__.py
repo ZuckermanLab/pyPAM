aa78b8c94cca9386c632cc26c6201658a85cbb87
August <30163079+augeorge@users.noreply.github.com>
2023-04-07 17:00:08 -0700

    up
    updates docs

--- pyPAM/__init__.py
+++ pyPAM/__init__.py
@@ -5,14 +5,19 @@ This method parallelizes multiple ensembles for better performance, and also inc
 
 
 example usage:
+Clone repository:
 ```bash
-pip install ...
-```
+git clone https://github.com/ZuckermanLab/pyPAM
 
+```
+Usage:
 ```python
-import
+import parallel_affine as pa
+sampler = pa.ParallelEnsembleSampler(n_ensembles, n_walkers, n_dim, log_prob, log_prob_args, thin, backend_fnames, moves)
+states = sampler.run_mixing_sampler(p_0, n_steps_list, n_cores, n_mixing_stages,thin,run_id_list)
+pa.plot_single_ensemble_mixing_distributions(sampler)  # quick plot distributions for each ensemble
+D = pau.get_data_all_runs(sampler, flat=True)  # get data --> shape = (n ensembles, n mixing stages, n_steps/thin, n_dim)
 ```
-
 
 
 '''
